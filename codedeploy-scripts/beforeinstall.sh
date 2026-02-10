7496d1c265e48c9d255842ce117ea59330a9edbf
David Hormigo Ramírez <dhorram948@g.educaand.es>
2026-02-10 09:56:29 +0100

    Ciclo completo

--- codedeploy-scripts/beforeinstall.sh
+++ codedeploy-scripts/beforeinstall.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-systemctl stop apache2
+mkdir -p /var/www/php
+cat << EOF > /etc/apache2/sites-available/php.conf
+<VirtualHost *:80>
+    DocumentRoot /var/www/php
+</VirtualHost>
+EOF
+a2dissite 000-default
+a2ensite php
